0a372fe995ad10a0bcc27f3c1c7fbf5d561f22aa
Rashad Hamid <mr_rashad@email.com>
2023-05-15 00:59:25 +0400

    changed

--- ResumeSpringBootWeb/src/main/java/com/company/UserController.java
+++ ResumeSpringBootWeb/src/main/java/com/company/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,20 +16,40 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserServiceinter userService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String index(HttpServletRequest request) throws ParseException {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        String email = request.getParameter("email");
-        String phone = request.getParameter("phone");
-        String address = request.getParameter("address");
-        String birthdateStr = request.getParameter("birthdate");
+    //    @RequestMapping(method = RequestMethod.GET)
+//    public String index(HttpServletRequest request) throws ParseException {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        String email = request.getParameter("email");
+//        String phone = request.getParameter("phone");
+//        String address = request.getParameter("address");
+//        String birthdateStr = request.getParameter("birthdate");
+//        java.sql.Date birthdate = null;
+//        if (birthdateStr != null && !birthdateStr.trim().isEmpty()) {
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//            Date bd = sdf.parse(birthdateStr);
+//            birthdate = new java.sql.Date(bd.getTime());
+//        }
+//
+//
+//        List<User> list = userService.getAll(name, surname, email, phone, address, birthdate);
+//        request.setAttribute("list", list);
+//        return "users";
+//    }
+    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    public ModelAndView index(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "surname", required = false) String surname,
+            @RequestParam(value = "email", required = false) String email,
+            @RequestParam(value = "phone", required = false) String phone,
+            @RequestParam(value = "address", required = false) String address,
+            @RequestParam(value = "birthdate", required = false) String birthdateStr) throws ParseException {
+
         java.sql.Date birthdate = null;
         if (birthdateStr != null && !birthdateStr.trim().isEmpty()) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -35,9 +57,15 @@ public class UserController {
             birthdate = new java.sql.Date(bd.getTime());
         }
 
-
         List<User> list = userService.getAll(name, surname, email, phone, address, birthdate);
-        request.setAttribute("list", list);
-        return "users";
+        ModelAndView mv = new ModelAndView("users");
+        mv.addObject("list", list);
+        return mv;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/usersm")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("users");
+        return mv;
     }
 }
